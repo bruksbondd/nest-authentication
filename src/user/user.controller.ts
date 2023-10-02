2584a23af68d78903333b99df3c97cc753907b32
Serhii Brekhov <bruksbond@gmail.com>
2023-10-02 15:18:29 +0300

    add api and dependesi

---
+++ src/user/user.controller.ts
@@ -0,0 +1,30 @@
+import {
+  Controller,
+  Post,
+  Delete,
+  Body,
+  Param,
+  Get,
+  ParseUUIDPipe,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  createUser(@Body() dto) {
+    return this.userService.save(dto);
+  }
+
+  @Get(':idOrEmail')
+  findOneUser(@Param('idOrEmail') idOrEmail: string) {
+    return this.userService.findOne(idOrEmail);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id', ParseUUIDPipe) id: string) {
+    return this.userService.delete(id);
+  }
+}
